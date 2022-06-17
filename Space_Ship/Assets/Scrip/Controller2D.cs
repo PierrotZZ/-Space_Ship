bea8d9ab6787ee2ed9930f8929cd29f8248f3ecd
PierrotZZ <101376982+PierrotZZ@users.noreply.github.com>
2022-06-18 03:38:29 +0700

    UpdateMovement2.5D

--- Space_Ship/Assets/Scrip/Controller2D.cs
+++ Space_Ship/Assets/Scrip/Controller2D.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Controller2D : MonoBehaviour
 {
     public CharacterController controller;
-
+    private Vector3 direction;
     public float speed = 6f;
 
     // Start is called before the first frame update
@@ -17,16 +17,14 @@ public class Controller2D : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
     }
 
     void Movement()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        Vector3 direction = new Vector3(horizontal, 0f, 0f).normalized;
-        float targetAngle = Mathf.Atan2(direction.x, 0f);
+        float horizontal = Input.GetAxis("Horizontal");
+        direction.x = horizontal * speed;
 
-        Vector3 moveDir = Quaternion.Euler(0f, 0f, 0f) * Vector3.forward;
-        controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        controller.Move(direction * Time.deltaTime);
     }
 }
